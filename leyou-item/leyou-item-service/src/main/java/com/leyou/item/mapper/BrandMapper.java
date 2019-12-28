4e8d77aa575871337f9d7105c70ec3708efb7749
Yi Zhou <zhouyi119119@gmail.com>
2019-12-28 17:14:41 +0800

    分类管理，品牌管理，controller，实体类建立

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -0,0 +1,10 @@
+package com.leyou.item.mapper;
+
+
+import com.leyou.item.pojo.Brand;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+@Repository
+public interface BrandMapper extends Mapper<Brand> {
+}
--- leyou-registry/src/main/java/com/leyou/LeyouRegistryApplication.java
+++ leyou-registry/src/main/java/com/leyou/LeyouRegistryApplication.java
@@ -4,6 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * spring cloud: 在springboot的基础上构建的微服务框架
+ *  eureka: 注册中心，服务的注册与发现
+ *      服务端：
+ *         1 引入服务端启动器，eureka-server
+ *         2 添加了配置 spring.application.name服务名
+ *                      eureka.client.service-url.defaultZone      -> http://localhost:10086/eruka
+ *         3 @EnableEurekaServer   开启eureka服务端功能
+ */
+
 @SpringBootApplication
 @EnableEurekaServer
 public class LeyouRegistryApplication {
