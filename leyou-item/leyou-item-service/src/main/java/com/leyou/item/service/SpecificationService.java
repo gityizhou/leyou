024f095a388d78726002943caf072e54236b23c3
Yi Zhou <zhouyi119119@gmail.com>
2020-01-04 11:12:03 +0800

    规格参数页面接口

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/SpecificationService.java
@@ -0,0 +1,37 @@
+package com.leyou.item.service;
+
+import com.leyou.item.mapper.SpecGroupMapper;
+import com.leyou.item.mapper.SpecParamMapper;
+import com.leyou.item.pojo.SpecGroup;
+import com.leyou.item.pojo.SpecParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SpecificationService {
+
+    @Autowired
+    private SpecGroupMapper groupMapper;
+
+    @Autowired
+    private SpecParamMapper paramMapper;
+
+    /**
+     * 根据分类id查询数组
+     * @param cid
+     * @return
+     */
+    public List<SpecGroup> queryGroupsByCid(Long cid) {
+        SpecGroup record = new SpecGroup();
+        record.setCid(cid);
+        return this.groupMapper.select(record);
+    }
+
+    public List<SpecParam> queryParams(Long gid) {
+        SpecParam specParam = new SpecParam();
+        specParam.setGroupId(gid);
+        return this.paramMapper.select(specParam);
+    }
+}
